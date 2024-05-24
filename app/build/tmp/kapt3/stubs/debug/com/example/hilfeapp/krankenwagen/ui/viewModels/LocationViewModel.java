cdf7e178075cc1c9fffc630042eec7de5a1cb23d
CurroCobain <fmunmar310@g.educaand.es>
2024-05-24 14:34:19 +0200

    Versión 1.2 hay que corregir fallo en camara del mapa, al iniciar urgencia deja de funcionar el boton "ir a"

--- app/build/tmp/kapt3/stubs/debug/com/example/hilfeapp/krankenwagen/ui/viewModels/LocationViewModel.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/hilfeapp/krankenwagen/ui/viewModels/LocationViewModel.java
@@ -10,7 +10,7 @@ package com.example.hilfeapp.krankenwagen.ui.viewModels;
  * @property focusErAmb indica dónde si enfoca el mapa
  * @property editUrg gestiona cuando se muestra el diálogo con la información de la urgencia
  */
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000N\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0010\u000b\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\b\u0007\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0006\u0010 \u001a\u00020!J\u0016\u0010\"\u001a\u00020!2\u0006\u0010#\u001a\u00020$2\u0006\u0010%\u001a\u00020\u001aJ\u0016\u0010\u001e\u001a\u00020!2\f\u0010&\u001a\b\u0012\u0004\u0012\u00020!0\'H\u0007J\u0006\u0010(\u001a\u00020!J\u0006\u0010)\u001a\u00020!J\u000e\u0010*\u001a\u00020!2\u0006\u0010+\u001a\u00020\u001aR \u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\b\u0010\t\"\u0004\b\n\u0010\u000bR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R \u0010\f\u001a\b\u0012\u0004\u0012\u00020\r0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000e\u0010\t\"\u0004\b\u000f\u0010\u000bR \u0010\u0010\u001a\b\u0012\u0004\u0012\u00020\r0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\t\"\u0004\b\u0012\u0010\u000bR\u001b\u0010\u0013\u001a\u00020\u00148BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0017\u0010\u0018\u001a\u0004\b\u0015\u0010\u0016R\"\u0010\u0019\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\u001a0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001b\u0010\t\"\u0004\b\u001c\u0010\u000bR\"\u0010\u001d\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\u001a0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001e\u0010\t\"\u0004\b\u001f\u0010\u000b\u00a8\u0006,"}, d2 = {"Lcom/example/hilfeapp/krankenwagen/ui/viewModels/LocationViewModel;", "Landroidx/lifecycle/ViewModel;", "context", "Landroid/content/Context;", "(Landroid/content/Context;)V", "addressText", "Lkotlinx/coroutines/flow/MutableStateFlow;", "", "getAddressText", "()Lkotlinx/coroutines/flow/MutableStateFlow;", "setAddressText", "(Lkotlinx/coroutines/flow/MutableStateFlow;)V", "editUrg", "", "getEditUrg", "setEditUrg", "focusErAmb", "getFocusErAmb", "setFocusErAmb", "fusedLocationClient", "Lcom/google/android/gms/location/FusedLocationProviderClient;", "getFusedLocationClient", "()Lcom/google/android/gms/location/FusedLocationProviderClient;", "fusedLocationClient$delegate", "Lkotlin/Lazy;", "urgencyLocation", "Lcom/google/android/gms/maps/model/LatLng;", "getUrgencyLocation", "setUrgencyLocation", "userLocation", "getUserLocation", "setUserLocation", "alterFocus", "", "getAddressFromCoordinates", "geocoder", "Landroid/location/Geocoder;", "locat", "onSuccess", "Lkotlin/Function0;", "openCloseEditUrg", "resetAddressText", "setUrLocation", "location", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000V\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u000b\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\b\b\u0007\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0006\u0010$\u001a\u00020%J\u0016\u0010&\u001a\u00020%2\u0006\u0010\'\u001a\u00020(2\u0006\u0010)\u001a\u00020\u001eJ\u0016\u0010\"\u001a\u00020%2\f\u0010*\u001a\b\u0012\u0004\u0012\u00020%0+H\u0007J\u0006\u0010,\u001a\u00020%J\u0006\u0010-\u001a\u00020%J\u000e\u0010.\u001a\u00020%2\u0006\u0010/\u001a\u00020\rJ\u000e\u00100\u001a\u00020%2\u0006\u00101\u001a\u00020\u001eJ\b\u00102\u001a\u00020%H\u0002R \u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\b\u0010\t\"\u0004\b\n\u0010\u000bR \u0010\f\u001a\b\u0012\u0004\u0012\u00020\r0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000e\u0010\t\"\u0004\b\u000f\u0010\u000bR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R \u0010\u0010\u001a\b\u0012\u0004\u0012\u00020\u00110\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\t\"\u0004\b\u0013\u0010\u000bR \u0010\u0014\u001a\b\u0012\u0004\u0012\u00020\u00110\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0015\u0010\t\"\u0004\b\u0016\u0010\u000bR\u001b\u0010\u0017\u001a\u00020\u00188BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u001b\u0010\u001c\u001a\u0004\b\u0019\u0010\u001aR\"\u0010\u001d\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\u001e0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001f\u0010\t\"\u0004\b \u0010\u000bR\"\u0010!\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\u001e0\u0006X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\"\u0010\t\"\u0004\b#\u0010\u000b\u00a8\u00063"}, d2 = {"Lcom/example/hilfeapp/krankenwagen/ui/viewModels/LocationViewModel;", "Landroidx/lifecycle/ViewModel;", "context", "Landroid/content/Context;", "(Landroid/content/Context;)V", "addressText", "Lkotlinx/coroutines/flow/MutableStateFlow;", "", "getAddressText", "()Lkotlinx/coroutines/flow/MutableStateFlow;", "setAddressText", "(Lkotlinx/coroutines/flow/MutableStateFlow;)V", "cameraPosition", "Lcom/google/maps/android/compose/CameraPositionState;", "getCameraPosition", "setCameraPosition", "editUrg", "", "getEditUrg", "setEditUrg", "focusErAmb", "getFocusErAmb", "setFocusErAmb", "fusedLocationClient", "Lcom/google/android/gms/location/FusedLocationProviderClient;", "getFusedLocationClient", "()Lcom/google/android/gms/location/FusedLocationProviderClient;", "fusedLocationClient$delegate", "Lkotlin/Lazy;", "urgencyLocation", "Lcom/google/android/gms/maps/model/LatLng;", "getUrgencyLocation", "setUrgencyLocation", "userLocation", "getUserLocation", "setUserLocation", "alterFocus", "", "getAddressFromCoordinates", "geocoder", "Landroid/location/Geocoder;", "locat", "onSuccess", "Lkotlin/Function0;", "openCloseEditUrg", "resetAddressText", "setCameraPositionState", "position", "setUrLocation", "location", "updateCameraPosition", "app_debug"})
 @android.annotation.SuppressLint(value = {"StaticFieldLeak"})
 public final class LocationViewModel extends androidx.lifecycle.ViewModel {
     @org.jetbrains.annotations.NotNull()
@@ -23,6 +23,8 @@ public final class LocationViewModel extends androidx.lifecycle.ViewModel {
     private kotlinx.coroutines.flow.MutableStateFlow<com.google.android.gms.maps.model.LatLng> userLocation;
     @org.jetbrains.annotations.NotNull()
     private kotlinx.coroutines.flow.MutableStateFlow<com.google.android.gms.maps.model.LatLng> urgencyLocation;
+    @org.jetbrains.annotations.NotNull()
+    private kotlinx.coroutines.flow.MutableStateFlow<com.google.maps.android.compose.CameraPositionState> cameraPosition;
     @org.jetbrains.annotations.NotNull()
     private kotlinx.coroutines.flow.MutableStateFlow<java.lang.Boolean> focusErAmb;
     @org.jetbrains.annotations.NotNull()
@@ -65,6 +67,15 @@ public final class LocationViewModel extends androidx.lifecycle.ViewModel {
     }
     
     @org.jetbrains.annotations.NotNull()
+    public final kotlinx.coroutines.flow.MutableStateFlow<com.google.maps.android.compose.CameraPositionState> getCameraPosition() {
+        return null;
+    }
+    
+    public final void setCameraPosition(@org.jetbrains.annotations.NotNull()
+    kotlinx.coroutines.flow.MutableStateFlow<com.google.maps.android.compose.CameraPositionState> p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
     public final kotlinx.coroutines.flow.MutableStateFlow<java.lang.Boolean> getFocusErAmb() {
         return null;
     }
@@ -86,6 +97,9 @@ public final class LocationViewModel extends androidx.lifecycle.ViewModel {
      * Método para cambiar el foco entre urgencia y ambulancia
      */
     public final void alterFocus() {
+    }
+    
+    private final void updateCameraPosition() {
     }
     
     /**
@@ -121,5 +135,9 @@ public final class LocationViewModel extends androidx.lifecycle.ViewModel {
      * Función que muestra u oculta el diálogo con la información de la urgencia
      */
     public final void openCloseEditUrg() {
+    }
+    
+    public final void setCameraPositionState(@org.jetbrains.annotations.NotNull()
+    com.google.maps.android.compose.CameraPositionState position) {
     }
 }
